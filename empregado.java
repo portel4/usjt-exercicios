a4784a311eeb6fb49f306a6ea26a86bf8c77e2e5
Luccas Portela <luccasaportela@gmail.com>
2020-09-03 20:36:25 -0300

    Commit inicial. Classe emregado foi criado

--- empregado.java
+++ empregado.java
@@ -1,6 +1,10 @@
 public class Empregado{
     public String nome;
+    public String tipo;
     public int idade;
+    private double salario;
+    private double comissao;
+    private double bonus;
 
     public String getNome(){
         return nome;
@@ -14,5 +18,18 @@ public class Empregado{
     }
     public void setIdade(int idade){
         this idade=idade;
+    }
+
+    public double calculoSalario(){
+        switch (tipo) {
+            case 1:     
+                return salario;
+            case 2:
+                return salario + salario * comissao;
+            case 3:
+                return salario + bonus;
+            default:
+                return 0;
+        }
     }
 }
